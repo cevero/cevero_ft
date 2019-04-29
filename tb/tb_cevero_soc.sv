b3a2d27d0a32bdc21b6fd4bb376e99a04f2488e3
otaviocomp <otavio10espsanto@gmail.com>
2019-04-29 20:34:20 -0300

    reorganized code

--- tb/tb_cevero_soc.sv
+++ tb/tb_cevero_soc.sv
@@ -3,22 +3,18 @@ module tb_cevero_ft;
     logic        clk_i;
     logic        rst_ni;
     logic        fetch_en_i;
-
-    logic        signal;
     logic [31:0] mem_flag;
     logic [31:0] mem_result;
     logic [31:0] instr_addr_0;
 
     soc dut
     (
-        .clk_i(clk_i),
-        .rst_ni(rst_ni),
-        .fetch_enable_i(fetch_en_i),
-
-        .signal(signal),
-        .mem_flag(mem_flag),
-        .mem_result(mem_result),
-        .instr_addr_0(instr_addr_0)
+        .clk_i          (clk_i       ),
+        .rst_ni         (rst_ni      ),
+        .fetch_enable_i (fetch_en_i  ),
+        .mem_flag       (mem_flag    ),
+        .mem_result     (mem_result  ),
+        .instr_addr_0   (instr_addr_0)
     );
 
     initial begin
@@ -31,8 +27,8 @@ module tb_cevero_ft;
     always #5 clk_i = ~clk_i;
       
     initial begin
-        $display("time | inst_addr_0 | mem_flag | mem_result | sig |\n");
-        $monitor ("%4t | %11h | %8b | %10d | %3b |", $time, instr_addr_0, mem_flag, mem_result, signal);
+        $display("time | inst_addr_0 | mem_flag | mem_result |\n");
+        $monitor ("%4t | %11h | %8b | %10d |", $time, instr_addr_0, mem_flag, mem_result);
          
         rst_ni = 0;
         fetch_en_i = 0;
