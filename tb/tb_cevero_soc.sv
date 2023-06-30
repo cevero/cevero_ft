55d84fa7fa21da38fdcffb32ff5a0c59a1eb9411
Matheus Dantas <matheus-dantas@hotmail.com>
2023-06-30 19:38:19 +0000

    updates

--- tb/tb_cevero_soc.sv
+++ tb/tb_cevero_soc.sv
@@ -59,15 +59,29 @@ module tb_cevero_ft;
 
 // ------------ ERROR INJECTION --------------
 
-    logic [31:0] reg_list[8] = {
+    logic [31:0] reg_list[22] = {
         dut.core.core_0.gen_regfile_ff.register_file_i.rf_reg[1],
-        dut.core.core_0.gen_regfile_ff.register_file_i.rf_reg[2],
-        dut.core.core_0.gen_regfile_ff.register_file_i.rf_reg[3],
-        dut.core.core_0.gen_regfile_ff.register_file_i.rf_reg[4],
+        dut.core.core_0.gen_regfile_ff.register_file_i.rf_reg[5],
+        dut.core.core_0.gen_regfile_ff.register_file_i.rf_reg[6],
+        dut.core.core_0.gen_regfile_ff.register_file_i.rf_reg[7],
+        dut.core.core_0.gen_regfile_ff.register_file_i.rf_reg[10],
+        dut.core.core_0.gen_regfile_ff.register_file_i.rf_reg[11],
+        dut.core.core_0.gen_regfile_ff.register_file_i.rf_reg[12],
+        dut.core.core_0.gen_regfile_ff.register_file_i.rf_reg[28],
+        dut.core.core_0.gen_regfile_ff.register_file_i.rf_reg[29],
+        dut.core.core_0.gen_regfile_ff.register_file_i.rf_reg[30],
+        dut.core.core_0.gen_regfile_ff.register_file_i.rf_reg[31],
         dut.core.core_1.gen_regfile_ff.register_file_i.rf_reg[1],
-        dut.core.core_1.gen_regfile_ff.register_file_i.rf_reg[2],
-        dut.core.core_1.gen_regfile_ff.register_file_i.rf_reg[3],
-        dut.core.core_1.gen_regfile_ff.register_file_i.rf_reg[4]
+        dut.core.core_1.gen_regfile_ff.register_file_i.rf_reg[5],
+        dut.core.core_1.gen_regfile_ff.register_file_i.rf_reg[6],
+        dut.core.core_1.gen_regfile_ff.register_file_i.rf_reg[7],
+        dut.core.core_1.gen_regfile_ff.register_file_i.rf_reg[10],
+        dut.core.core_1.gen_regfile_ff.register_file_i.rf_reg[11],
+        dut.core.core_1.gen_regfile_ff.register_file_i.rf_reg[12],
+        dut.core.core_1.gen_regfile_ff.register_file_i.rf_reg[28],
+        dut.core.core_1.gen_regfile_ff.register_file_i.rf_reg[29],
+        dut.core.core_1.gen_regfile_ff.register_file_i.rf_reg[30],
+        dut.core.core_1.gen_regfile_ff.register_file_i.rf_reg[31]
     };
 
     int index;
@@ -108,7 +122,7 @@ module tb_cevero_ft;
         // seed, mean and sd
         nbits = $dist_normal(5, 0, 4);
         if (err == 0)
-            index = $urandom_range(0, 7);
+            index = $urandom_range(0, 21);
             
         if (can_inject_error) begin
             $display("Valor gerado %d", nbits);
@@ -118,25 +132,53 @@ module tb_cevero_ft;
             
             unique case (index)
                 0 : force dut.core.core_0.gen_regfile_ff.register_file_i.rf_reg[1] = random_error_generator(dut.core.core_0.gen_regfile_ff.register_file_i.rf_reg[1], nbits); 
-                1 : force dut.core.core_0.gen_regfile_ff.register_file_i.rf_reg[1] = random_error_generator(dut.core.core_0.gen_regfile_ff.register_file_i.rf_reg[2], nbits); 
-                2 : force dut.core.core_0.gen_regfile_ff.register_file_i.rf_reg[1] = random_error_generator(dut.core.core_0.gen_regfile_ff.register_file_i.rf_reg[3], nbits); 
-                3 : force dut.core.core_0.gen_regfile_ff.register_file_i.rf_reg[1] = random_error_generator(dut.core.core_0.gen_regfile_ff.register_file_i.rf_reg[4], nbits); 
-                4 : force dut.core.core_1.gen_regfile_ff.register_file_i.rf_reg[1] = random_error_generator(dut.core.core_1.gen_regfile_ff.register_file_i.rf_reg[1], nbits); 
-                5 : force dut.core.core_1.gen_regfile_ff.register_file_i.rf_reg[1] = random_error_generator(dut.core.core_1.gen_regfile_ff.register_file_i.rf_reg[2], nbits); 
-                6 : force dut.core.core_1.gen_regfile_ff.register_file_i.rf_reg[1] = random_error_generator(dut.core.core_1.gen_regfile_ff.register_file_i.rf_reg[3], nbits); 
-                7 : force dut.core.core_1.gen_regfile_ff.register_file_i.rf_reg[1] = random_error_generator(dut.core.core_1.gen_regfile_ff.register_file_i.rf_reg[4], nbits); 
+                1 : force dut.core.core_0.gen_regfile_ff.register_file_i.rf_reg[5] = random_error_generator(dut.core.core_0.gen_regfile_ff.register_file_i.rf_reg[5], nbits); 
+                2 : force dut.core.core_0.gen_regfile_ff.register_file_i.rf_reg[6] = random_error_generator(dut.core.core_0.gen_regfile_ff.register_file_i.rf_reg[6], nbits); 
+                3 : force dut.core.core_0.gen_regfile_ff.register_file_i.rf_reg[7] = random_error_generator(dut.core.core_0.gen_regfile_ff.register_file_i.rf_reg[7], nbits); 
+                4 : force dut.core.core_0.gen_regfile_ff.register_file_i.rf_reg[10] = random_error_generator(dut.core.core_0.gen_regfile_ff.register_file_i.rf_reg[10], nbits); 
+                5 : force dut.core.core_0.gen_regfile_ff.register_file_i.rf_reg[11] = random_error_generator(dut.core.core_0.gen_regfile_ff.register_file_i.rf_reg[11], nbits); 
+                6 : force dut.core.core_0.gen_regfile_ff.register_file_i.rf_reg[12] = random_error_generator(dut.core.core_0.gen_regfile_ff.register_file_i.rf_reg[12], nbits); 
+                7 : force dut.core.core_0.gen_regfile_ff.register_file_i.rf_reg[28] = random_error_generator(dut.core.core_0.gen_regfile_ff.register_file_i.rf_reg[28], nbits); 
+                8 : force dut.core.core_0.gen_regfile_ff.register_file_i.rf_reg[29] = random_error_generator(dut.core.core_0.gen_regfile_ff.register_file_i.rf_reg[29], nbits); 
+                9 : force dut.core.core_0.gen_regfile_ff.register_file_i.rf_reg[30] = random_error_generator(dut.core.core_0.gen_regfile_ff.register_file_i.rf_reg[30], nbits); 
+                10 : force dut.core.core_0.gen_regfile_ff.register_file_i.rf_reg[31] = random_error_generator(dut.core.core_0.gen_regfile_ff.register_file_i.rf_reg[31], nbits);
+                11 : force dut.core.core_1.gen_regfile_ff.register_file_i.rf_reg[1] = random_error_generator(dut.core.core_1.gen_regfile_ff.register_file_i.rf_reg[1], nbits); 
+                12 : force dut.core.core_1.gen_regfile_ff.register_file_i.rf_reg[5] = random_error_generator(dut.core.core_1.gen_regfile_ff.register_file_i.rf_reg[5], nbits); 
+                13 : force dut.core.core_1.gen_regfile_ff.register_file_i.rf_reg[6] = random_error_generator(dut.core.core_1.gen_regfile_ff.register_file_i.rf_reg[6], nbits); 
+                14 : force dut.core.core_1.gen_regfile_ff.register_file_i.rf_reg[7] = random_error_generator(dut.core.core_1.gen_regfile_ff.register_file_i.rf_reg[7], nbits); 
+                15 : force dut.core.core_1.gen_regfile_ff.register_file_i.rf_reg[10] = random_error_generator(dut.core.core_1.gen_regfile_ff.register_file_i.rf_reg[10], nbits); 
+                16 : force dut.core.core_1.gen_regfile_ff.register_file_i.rf_reg[11] = random_error_generator(dut.core.core_1.gen_regfile_ff.register_file_i.rf_reg[11], nbits); 
+                17 : force dut.core.core_1.gen_regfile_ff.register_file_i.rf_reg[12] = random_error_generator(dut.core.core_1.gen_regfile_ff.register_file_i.rf_reg[12], nbits); 
+                18 : force dut.core.core_1.gen_regfile_ff.register_file_i.rf_reg[28] = random_error_generator(dut.core.core_1.gen_regfile_ff.register_file_i.rf_reg[28], nbits); 
+                19 : force dut.core.core_1.gen_regfile_ff.register_file_i.rf_reg[29] = random_error_generator(dut.core.core_1.gen_regfile_ff.register_file_i.rf_reg[29], nbits); 
+                20 : force dut.core.core_1.gen_regfile_ff.register_file_i.rf_reg[30] = random_error_generator(dut.core.core_1.gen_regfile_ff.register_file_i.rf_reg[30], nbits); 
+                21 : force dut.core.core_1.gen_regfile_ff.register_file_i.rf_reg[31] = random_error_generator(dut.core.core_1.gen_regfile_ff.register_file_i.rf_reg[31], nbits); 
             endcase
         end
         else begin
-            unique case (index)
-                0 : release dut.core.core_0.gen_regfile_ff.register_file_i.rf_reg[1]; 
-                1 : release dut.core.core_0.gen_regfile_ff.register_file_i.rf_reg[2];  
-                2 : release dut.core.core_0.gen_regfile_ff.register_file_i.rf_reg[3];  
-                3 : release dut.core.core_0.gen_regfile_ff.register_file_i.rf_reg[4];  
-                4 : release dut.core.core_1.gen_regfile_ff.register_file_i.rf_reg[1];  
-                5 : release dut.core.core_1.gen_regfile_ff.register_file_i.rf_reg[2];  
-                6 : release dut.core.core_1.gen_regfile_ff.register_file_i.rf_reg[3];  
-                7 : release dut.core.core_1.gen_regfile_ff.register_file_i.rf_reg[4];  
+            unique case (index) 
+                0 : release dut.core.core_0.gen_regfile_ff.register_file_i.rf_reg[1];
+                1 : release dut.core.core_0.gen_regfile_ff.register_file_i.rf_reg[5];
+                2 : release dut.core.core_0.gen_regfile_ff.register_file_i.rf_reg[6];
+                3 : release dut.core.core_0.gen_regfile_ff.register_file_i.rf_reg[7];
+                4 : release dut.core.core_0.gen_regfile_ff.register_file_i.rf_reg[10];
+                5 : release dut.core.core_0.gen_regfile_ff.register_file_i.rf_reg[11];
+                6 : release dut.core.core_0.gen_regfile_ff.register_file_i.rf_reg[12];
+                7 : release dut.core.core_0.gen_regfile_ff.register_file_i.rf_reg[28];
+                8 : release dut.core.core_0.gen_regfile_ff.register_file_i.rf_reg[29];
+                9 : release dut.core.core_0.gen_regfile_ff.register_file_i.rf_reg[30];
+                10 : release dut.core.core_0.gen_regfile_ff.register_file_i.rf_reg[31];
+                11 : release dut.core.core_1.gen_regfile_ff.register_file_i.rf_reg[1];
+                12 : release dut.core.core_1.gen_regfile_ff.register_file_i.rf_reg[5];
+                13 : release dut.core.core_1.gen_regfile_ff.register_file_i.rf_reg[6];
+                14 : release dut.core.core_1.gen_regfile_ff.register_file_i.rf_reg[7];
+                15 : release dut.core.core_1.gen_regfile_ff.register_file_i.rf_reg[10];
+                16 : release dut.core.core_1.gen_regfile_ff.register_file_i.rf_reg[11];
+                17 : release dut.core.core_1.gen_regfile_ff.register_file_i.rf_reg[12];
+                18 : release dut.core.core_1.gen_regfile_ff.register_file_i.rf_reg[28];
+                19 : release dut.core.core_1.gen_regfile_ff.register_file_i.rf_reg[29];
+                20 : release dut.core.core_1.gen_regfile_ff.register_file_i.rf_reg[30];
+                21 : release dut.core.core_1.gen_regfile_ff.register_file_i.rf_reg[31];
             endcase
 
             err = 0;
